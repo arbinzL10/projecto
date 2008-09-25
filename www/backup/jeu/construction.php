9537d825c67a58e3e3fc43f56e8b0c906cc0e859
kirubb <kirubb@b42c3602-896a-11dd-82a6-aba1b78984bd>
2008-09-25 16:02:11 +0000

    cleaning

--- www/backup/jeu/construction.php
+++
@@ -1,5 +0,0 @@
-<?php include('functions_nico.php');
-
-construire($_SESSION['id'], $_POST['batiment'], $_POST['nbbat']);
-
-?>
--- www/backup/jeu/recrutement.php
+++
@@ -1,6 +0,0 @@
-<?php 
-session_start(); 
-include('functions_nico.php');
-recruter($_SESSION['id'], $_POST['unit'], $_POST['nbunit']);
-
-?>
